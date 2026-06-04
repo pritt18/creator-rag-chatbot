84f656d9fd43fad90fbe3a4b0cf6280d05b554a3
pritt18 <pritamgangurde18@gmail.com>
2026-06-04 19:37:40 +0530

    Handle YouTube metadata errors

--- backend/metadata.py
+++ backend/metadata.py
@@ -6,37 +6,54 @@ def get_youtube_metadata(url):
         "quiet": True
     }
 
-    with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(
-            url,
-            download=False
-        )
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(
+                url,
+                download=False
+            )
 
-    title = info.get("title")
-    views = info.get("view_count", 0)
-    likes = info.get("like_count", 0)
-    comments = info.get("comment_count", 0)
-    creator = info.get("channel")
-    duration = info.get("duration")
-    upload_date = info.get("upload_date")
-    thumbnail = info.get("thumbnail")
+        title = info.get("title")
+        views = info.get("view_count", 0)
+        likes = info.get("like_count", 0)
+        comments = info.get("comment_count", 0)
+        creator = info.get("channel")
+        duration = info.get("duration")
+        upload_date = info.get("upload_date")
+        thumbnail = info.get("thumbnail")
 
-    engagement_rate = 0
+        engagement_rate = 0
 
-    if views:
-        engagement_rate = round(
-            ((likes + comments) / views) * 100,
-            2
-        )
+        if views:
+            engagement_rate = round(
+                ((likes + comments) / views) * 100,
+                2
+            )
 
-    return {
-        "title": title,
-        "views": views,
-        "likes": likes,
-        "comments": comments,
-        "creator": creator,
-        "duration": duration,
-        "upload_date": upload_date,
-        "engagement_rate": engagement_rate,
-        "thumbnail": thumbnail
-    }
+        return {
+            "title": title,
+            "views": views,
+            "likes": likes,
+            "comments": comments,
+            "creator": creator,
+            "duration": duration,
+            "upload_date": upload_date,
+            "engagement_rate": engagement_rate,
+            "thumbnail": thumbnail
+        }
+
+    except Exception as e:
+        print("YouTube Metadata Error:", str(e))
+
+        return {
+            "title": "Unavailable",
+            "views": 0,
+            "likes": 0,
+            "comments": 0,
+            "creator": "Unavailable",
+            "duration": 0,
+            "upload_date": "",
+            "engagement_rate": 0,
+            "thumbnail": "",
+            "error": str(e)
+        }
